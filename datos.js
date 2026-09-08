1d62255b82c63f4c811e9e25820886acf93372f3
martinabahamondes-debug <martina.bahamondes@ug.uchile.cl>
2026-09-07 23:09:11 -0300

    Primera carga de tarea 1

---
+++ datos.js
@@ -0,0 +1,87 @@
+const avistamientos = [
+
+  {
+    nombre: "Garza cuca",
+    tipo: "acuatica",
+    tipoTexto: "Acuática",
+
+    region: "Los Ríos",
+    comuna: "Valdivia",
+    lugar: "Humedal Angachilla",
+
+    fecha: "2026-09-05",
+    hora: "11:20",
+
+    tipoArchivo: "imagen",
+    archivo: "img/garza-cuca.jpg"
+  },
+
+
+  {
+    nombre: "Loica",
+    tipo: "paseriforme",
+    tipoTexto: "Ave cantora / paseriforme",
+
+    region: "Región Metropolitana",
+    comuna: "Pirque",
+    lugar: "Sector rural de Pirque",
+
+    fecha: "2026-09-03",
+    hora: "09:45",
+
+    tipoArchivo: "imagen",
+    archivo: "img/loica.jpg"
+  },
+
+
+  {
+    nombre: "Chincol",
+    tipo: "paseriforme",
+    tipoTexto: "Ave cantora / paseriforme",
+
+    region: "Región Metropolitana",
+    comuna: "La Reina",
+    lugar: "Parque Mahuida",
+
+    fecha: "2026-08-31",
+    hora: "10:15",
+
+    tipoArchivo: "imagen",
+    archivo: "img/chincol.jpg"
+  },
+
+
+  {
+    nombre: "Tiuque",
+    tipo: "rapaz_diurna",
+    tipoTexto: "Rapaz diurna",
+
+    region: "La Araucanía",
+    comuna: "Pucón",
+    lugar: "Sector rural de Pucón",
+
+    fecha: "2026-08-28",
+    hora: "14:30",
+
+    tipoArchivo: "imagen",
+    archivo: "img/tiuque.jpg"
+  },
+
+
+  {
+    nombre: "Tenca",
+    tipo: "paseriforme",
+    tipoTexto: "Ave cantora / paseriforme",
+
+    region: "Valparaíso",
+    comuna: "Olmué",
+    lugar: "Sector Parque Nacional La Campana",
+
+    fecha: "2026-08-25",
+    hora: "16:40",
+
+    tipoArchivo: "imagen",
+    archivo: "img/tenca.jpg"
+  }
+
+];
